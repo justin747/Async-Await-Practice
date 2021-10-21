60d4f8667f931f64d4498030456e0c34e29410b4
Justin747 <justin747@gmail.com>
2021-10-20 20:26:28 -0700

    Initial Commit

---
+++ Async Await Practice/Async_Await_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_PracticeApp.swift
+//  Async Await Practice
+//
+//  Created by Justin747 on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
