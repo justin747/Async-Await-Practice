bfdd2d014a209ce720f9c4a4aa666f4a465a3e7e
Justin747 <justin747@gmail.com>
2021-10-27 21:05:46 -0700

    Added "ephemeral" modifier
    Added "ephemeral" modifier to stop caching of data to test error handling when API is not available

--- Async Await Practice/Service/CharacterService.swift
+++ Async Await Practice/Service/CharacterService.swift
@@ -20,7 +20,9 @@ struct CharacterService {
     func fetchCharacters() async throws -> [Character] {
         
         let url = URL(string: "https://rickandmortyapi.com/api/character/")!
-        let (data, response) = try await URLSession.shared.data(from: url)
+        let configuration = URLSessionConfiguration.ephemeral
+        
+        let (data, response) = try await URLSession(configuration: configuration).data(from: url)
         
         guard let response = response as? HTTPURLResponse,
               response.statusCode == 200 else {
